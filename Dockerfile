5a0b4e50701dfebe96793bd6dd9429a573f2c176
DariushDD <github.provoke706@passinbox.com>
2025-10-05 19:39:13 +0330

    nn

--- Dockerfile
+++ Dockerfile
@@ -1,46 +1,24 @@
-# Start from Alpine base image
-FROM alpine
-LABEL maintainer="Mr.Philipp <d3vilh@github.com>"
-LABEL version="0.5.5"
+# Base image
+FROM alpine:3.18
 
-# Set the working directory to /opt/app
-WORKDIR /opt/app
+# Install required packages
+RUN apk --no-cache add bash curl iptables ip6tables openvpn easy-rsa
 
-COPY config/easy-rsa.vars /etc/openvpn/config/easy-rsa.vars
+# Set environment variables for EasyRSA
+ENV EASYRSA="/usr/share/easy-rsa"
+ENV OVPN_DIR="/etc/openvpn"
 
-RUN apk --no-cache --no-progress upgrade && apk --no-cache --no-progress add \
-    bash \
-    bind-tools \
-    oath-toolkit-oathtool \
-    curl \
-    ip6tables \
-    iptables \
-    openvpn \
-    easy-rsa
-
-
-#Install Latest RasyRSA Version
-RUN chmod 755 /usr/share/easy-rsa/*
-
-# Copy all files in the current directory to the /opt/app directory in the container
+# Copy scripts and configuration
 COPY bin /opt/app/bin
 COPY docker-entrypoint.sh /opt/app/docker-entrypoint.sh
-RUN mkdir -p /opt/app/clients \
-    /opt/app/db \
-    /opt/app/log \
-    /opt/app/pki \
-    /opt/app/staticclients \
-    /opt/app/config
+COPY config/easy-rsa.vars /etc/openvpn/config/easy-rsa.vars
 
-# Add the openssl-easyrsa.cnf file to the easy-rsa directory
-ADD openssl-easyrsa.cnf /opt/app/easy-rsa/
+# Make scripts executable
+RUN chmod +x /opt/app/bin/* /opt/app/docker-entrypoint.sh \
+    && mkdir -p /opt/app/clients /opt/app/pki /opt/app/config /opt/app/log
 
-# Make all files in the bin directory executable
-RUN chmod +x bin/*; chmod +x docker-entrypoint.sh
-
-# Expose the OpenVPN port (1194/udp)
+# Expose UDP port for OpenVPN
 EXPOSE 1194/udp
 
-# Set the entrypoint to the docker-entrypoint.sh script, passing in the following arguments:
-# $REQ_COUNTRY $REQ_PROVINCE $REQ_CITY $REQ_ORG $REQ_OU $REQ_CN
-ENTRYPOINT ./docker-entrypoint.sh
+# Set entrypoint
+ENTRYPOINT ["/opt/app/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,97 +1,73 @@
 #!/bin/bash
-#VERSION 0.2.3 by @d3vilh@github.com aka Mr. Philipp
 set -e
 
-#Variables
-EASY_RSA=/usr/share/easy-rsa
-OPENVPN_DIR=/etc/openvpn
-echo "EasyRSA path: $EASY_RSA OVPN path: $OPENVPN_DIR"
+EASYRSA="/usr/share/easy-rsa"
+OVPN_DIR="/etc/openvpn"
+CLIENT_NAME="client1"
 
-if [[ ! -f $OPENVPN_DIR/pki/ca.crt ]]; then
-    export EASYRSA_BATCH=1 # see https://superuser.com/questions/1331293/easy-rsa-v3-execute-build-ca-and-gen-req-silently
-    cd $EASY_RSA
-
-    # Building the CA
-    echo 'Setting up public key infrastructure...'
-    $EASY_RSA/easyrsa init-pki
-
-    # Copy easy-rsa variables
-    cp $OPENVPN_DIR/config/easy-rsa.vars $EASY_RSA/pki/vars
-
-    # Listing env parameters:
-    echo "Following EASYRSA variables will be used:"
-    cat $EASY_RSA/pki/vars | awk '{$1=""; print $0}';
-
-    echo 'Generating ertificate authority...'
-    $EASY_RSA/easyrsa build-ca nopass
-
-    # Creating the Server Certificate, Key, and Encryption Files
-    echo 'Creating the Server Certificate...'
-    $EASY_RSA/easyrsa gen-req server nopass
-
-    echo 'Sign request...'
-    $EASY_RSA/easyrsa sign-req server server
-
-    echo 'Generate Diffie-Hellman key...'
-    $EASY_RSA/easyrsa gen-dh
-
-    echo 'Generate HMAC signature...'
-    openvpn --genkey --secret $EASY_RSA/pki/ta.key
-
-    echo 'Create certificate revocation list (CRL)...'
-    $EASY_RSA/easyrsa gen-crl
-    chmod +r $EASY_RSA/pki/crl.pem
-
-    # Copy to mounted volume
-    cp -r $EASY_RSA/pki/. $OPENVPN_DIR/pki
-else
-
-    echo 'PKI already set up.'
+# Initialize PKI if not exists
+if [ ! -d "$EASYRSA/pki" ]; then
+    echo "Initializing PKI..."
+    $EASYRSA/easyrsa init-pki
 fi
 
-# Listing env parameters:
-echo "Following EASYRSA variables were set during CA init:"
-cat $OPENVPN_DIR/pki/vars | awk '{$1=""; print $0}';
-
-# Configure network
-mkdir -p /dev/net
-if [ ! -c /dev/net/tun ]; then
-    mknod /dev/net/tun c 10 200
+# Build CA if not exists
+if [ ! -f "$EASYRSA/pki/ca.crt" ]; then
+    echo "Building CA..."
+    cd $EASYRSA
+    ./easyrsa --batch build-ca nopass
 fi
 
-echo 'Configuring networking rules...'
-if ! grep -q 'net.ipv4.ip_forward=1' /etc/sysctl.conf; then
-  echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf; 
-  echo 'IP forwarding configuration now applied:'
-else
-  echo 'IP forwarding configuration already applied:'
-fi
-sysctl -p /etc/sysctl.conf
-
-echo 'Configuring iptables...'
-echo 'NAT for OpenVPN clients'
-iptables -t nat -A POSTROUTING -s $TRUST_SUB -o eth0 -j MASQUERADE
-iptables -t nat -A POSTROUTING -s $GUEST_SUB -o eth0 -j MASQUERADE
-
-echo 'Blocking ICMP for external clients'
-iptables -A FORWARD -p icmp -j DROP --icmp-type echo-request -s $GUEST_SUB 
-iptables -A FORWARD -p icmp -j DROP --icmp-type echo-reply -s $GUEST_SUB 
-
-echo 'Blocking internal home subnet to access from external openvpn clients (Internet still available)'
-iptables -A FORWARD -s $GUEST_SUB -d $HOME_SUB -j DROP
-
-if [[ ! -s fw-rules.sh ]]; then
-    echo "No additional firewall rules to apply."
-else
-    echo "Applying firewall rules"
-    ./fw-rules.sh
-    echo 'Additional firewall rules applied.'
+# Build Server Certificate
+if [ ! -f "$EASYRSA/pki/issued/server.crt" ]; then
+    echo "Building Server Certificate..."
+    cd $EASYRSA
+    ./easyrsa build-server-full server nopass
+    ./easyrsa gen-dh
+    openvpn --genkey --secret $OVPN_DIR/ta.key
 fi
 
-echo 'IPT MASQ Chains:'
-iptables -t nat -L | grep MASQ
-echo 'IPT FWD Chains:'
-iptables -v -x -n -L | grep DROP 
+# Build Client Certificate
+cd $EASYRSA
+if [ ! -f "$EASYRSA/pki/issued/$CLIENT_NAME.crt" ]; then
+    echo "Building Client Certificate..."
+    ./easyrsa build-client-full $CLIENT_NAME nopass
+fi
 
-echo 'Start openvpn process...'
-/usr/sbin/openvpn --cd $OPENVPN_DIR --script-security 2 --config $OPENVPN_DIR/server.conf
+# Generate client.ovpn
+CLIENT_OVPN="/opt/app/clients/$CLIENT_NAME.ovpn"
+if [ ! -f "$CLIENT_OVPN" ]; then
+    echo "Generating client.ovpn..."
+    cat > $CLIENT_OVPN <<EOL
+client
+dev tun
+proto udp
+remote YOUR_SERVER_IP 1194
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+remote-cert-tls server
+cipher AES-256-CBC
+auth SHA256
+key-direction 1
+verb 3
+
+<ca>
+$(cat $EASYRSA/pki/ca.crt)
+</ca>
+<cert>
+$(cat $EASYRSA/pki/issued/$CLIENT_NAME.crt)
+</cert>
+<key>
+$(cat $EASYRSA/pki/private/$CLIENT_NAME.key)
+</key>
+<tls-auth>
+$(cat $OVPN_DIR/ta.key)
+</tls-auth>
+EOL
+fi
+
+# Start OpenVPN server
+echo "Starting OpenVPN server..."
+openvpn --config $OVPN_DIR/server.conf
